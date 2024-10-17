2f740be67ff21c6d01d949188cb0a66ef214a734
ktung4 <ktung@chicagobooth.edu>
2024-10-17 00:49:08 +0000

    Umbrella

--- umbrella.rb
+++ umbrella.rb
@@ -10,31 +10,25 @@ user_location = gets.chomp
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
-
 resp = HTTP.get (maps_url)
-
 raw_response = resp.to_s
-
 parse_response = JSON.parse(raw_response)
-
 results = parse_response.fetch("results")
-
 first_result = results.at(0)
-
 geo = first_result.fetch("geometry")
-
 loc = geo.fetch("location")
-
 pp latitude = loc.fetch("lat")
-
 pp longitude = loc.fetch("lng")
 
+pirate_weather_api_key=ENV.fetch("PIRATE_WEATHER_KEY")
+pirate_weather_url = "https://api.pirateweather.net/forecast/"+pirate_weather_api_key+"/"+latitude.to_s+","+longitude.to_s
+raw_response = HTTP.get(pirate_weather_url)
+
+
+#https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 # require "http"
-
 # pirate_weather_api_key=ENV.fetch("PIRATE_WEATHER_KEY")
-
 # pirate_weather_url = "https://apipirateweather.net/forecast/"+pirate_weather_api_key+"/41.8887,-87.6355"
-
 # raw_response = HTTP.get(pirate_weather_url)
 
 # require "json"
